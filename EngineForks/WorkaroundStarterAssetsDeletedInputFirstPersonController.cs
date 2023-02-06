d83d9b71a55ceae3479fa687c7c561a168abfc73
adamgit <adam.m.s.martin+github@gmail.com>
2023-02-06 01:07:07 +0000

    ADDED: pair of classes that fix Unity's own FirstPersonController, re-instating support for 'legacy' (ie production ready) Input

---
+++ EngineForks/WorkaroundStarterAssetsDeletedInputFirstPersonController.cs
@@ -0,0 +1,266 @@
+﻿using UnityEngine;
+
+namespace PublishersFork
+{
+	/// <summary>
+	/// In 2022, despite the fact that "New Input" still isn't fully implemented, and is FULL of serious bugs,
+	/// Unity staff deleted all Input code from their official assets provided to Publishers for use in demos.
+	///
+	/// This class is a <b>minimally</b> edited version of the (now broken) FirstPersonController.cs class - as of 2022,
+	/// Unity's staff inserted a runtime deliberate "stop the app" line of code rather than fix/maintain their
+	/// existing Input code.
+	///
+	/// NOTE: this class also removes dependency on CineMachine, which is unfortunate, but was the fastest/easiest way
+	/// to re-instate the production-quality existing Input system. If you need CineMachine you'll need to tweak it
+	/// yourself and decide how you're going to translate the traditional Inputs into CineMachine effects.
+	///
+	/// Install instructions:
+	/// 1. on the "PlayerCapsule" GameObject, replace the Monobehaviour "FirstPersonController" with
+	/// this MonoBehaviour
+	/// 2. on the "PlayerCapsule" GameObject, replace the Monobehaviour "StarterAssetsInputs" with
+	/// <see cref="WorkaroundStarterAssetsDeletedInputStarterAssetsInputs"/>
+	/// 3. Add a "Camera" MonoBehaviour to your "PlayerCameraRoot" GameObject (child of the "PlayerCapsule")
+	/// 4. Change the new Camera's tag to "MainCamera"
+	/// 5. Delete the other CineMachine objects from your scene.
+	///
+	/// NOTE: This REQUIRES you to also use the class <see cref="WorkaroundStarterAssetsDeletedInputStarterAssetsInputs"/>
+	/// since Unity's design splits the implementation into two classes.
+	///
+	/// NOTE: This REQUIRES you to download/install the official (free) Package: https://assetstore.unity.com/packages/essentials/starter-assets-first-person-character-controller-196525
+	///
+	/// NOTE: If you want to support both old and new input then it is easy to take the modifications in this
+	/// pair of classes and add them to Unity's official classes - use "diff" to see exactly which lines of code
+	/// needed modification. Unity staff could have (should have!) done this themselves but chose not to.
+	/// </summary>
+	public class WorkaroundStarterAssetsDeletedInputFirstPersonController : MonoBehaviour
+	{
+		[Header("Player")]
+		[Tooltip("Move speed of the character in m/s")]
+		public float MoveSpeed = 4.0f;
+		[Tooltip("Sprint speed of the character in m/s")]
+		public float SprintSpeed = 6.0f;
+		[Tooltip("Rotation speed of the character")]
+		public float RotationSpeed = 1.0f;
+		[Tooltip("Acceleration and deceleration")]
+		public float SpeedChangeRate = 10.0f;
+
+		[Space(10)]
+		[Tooltip("The height the player can jump")]
+		public float JumpHeight = 1.2f;
+		[Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
+		public float Gravity = -15.0f;
+
+		[Space(10)]
+		[Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
+		public float JumpTimeout = 0.1f;
+		[Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
+		public float FallTimeout = 0.15f;
+
+		[Header("Player Grounded")]
+		[Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
+		public bool Grounded = true;
+		[Tooltip("Useful for rough ground")]
+		public float GroundedOffset = -0.14f;
+		[Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
+		public float GroundedRadius = 0.5f;
+		[Tooltip("What layers the character uses as ground")]
+		public LayerMask GroundLayers;
+
+		[Tooltip("How far in degrees can you move the camera up")]
+		public float TopClamp = 90.0f;
+		[Tooltip("How far in degrees can you move the camera down")]
+		public float BottomClamp = -90.0f;
+
+		// cinemachine
+		private float _cinemachineTargetPitch;
+
+		// player
+		private float _speed;
+		private float _rotationVelocity;
+		private float _verticalVelocity;
+		private float _terminalVelocity = 53.0f;
+
+		// timeout deltatime
+		private float _jumpTimeoutDelta;
+		private float _fallTimeoutDelta;
+
+	
+		private CharacterController _controller;
+		private WorkaroundStarterAssetsDeletedInputStarterAssetsInputs _input;
+		private GameObject _mainCamera;
+
+		private const float _threshold = 0.01f;
+
+		private void Awake()
+		{
+			// get a reference to our main camera
+			if (_mainCamera == null)
+			{
+				_mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+			}
+		}
+
+		private void Start()
+		{
+			_controller = GetComponent<CharacterController>();
+			_input = GetComponent<WorkaroundStarterAssetsDeletedInputStarterAssetsInputs>();
+
+			// reset our timeouts on start
+			_jumpTimeoutDelta = JumpTimeout;
+			_fallTimeoutDelta = FallTimeout;
+		}
+
+		private void Update()
+		{
+			_input.SampleInput();
+			JumpAndGravity();
+			GroundedCheck();
+			Move();
+		}
+
+		private void LateUpdate()
+		{
+			CameraRotation();
+		}
+
+		private void GroundedCheck()
+		{
+			// set sphere position, with offset
+			Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z);
+			Grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers, QueryTriggerInteraction.Ignore);
+		}
+
+		private void CameraRotation()
+		{
+			// if there is an input
+			if (_input.look.sqrMagnitude >= _threshold)
+			{
+				//Don't multiply ANY input by Time.deltaTime, Unity. Also note the Unity employees who don't know what the word 'velocity' literally means!
+				_rotationVelocity = _input.look.x * RotationSpeed;
+
+				// clamp our pitch rotation
+				_cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+
+				// Update Cinemachine camera target pitch
+				_mainCamera.transform.Rotate( Vector3.right * _input.look.y * -1f * RotationSpeed );
+
+				// rotate the player left and right
+				transform.Rotate(Vector3.up * _rotationVelocity);
+			}
+		}
+
+		private void Move()
+		{
+			// set target speed based on move speed, sprint speed and if sprint is pressed
+			float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
+
+			// a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
+
+			// note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+			// if there is no input, set the target speed to 0
+			if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+
+			// a reference to the players current horizontal velocity
+			float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
+
+			float speedOffset = 0.1f;
+			float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
+
+			// accelerate or decelerate to target speed
+			if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
+			{
+				// creates curved result rather than a linear one giving a more organic speed change
+				// note T in Lerp is clamped, so we don't need to clamp our speed
+				_speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * SpeedChangeRate);
+
+				// round speed to 3 decimal places
+				_speed = Mathf.Round(_speed * 1000f) / 1000f;
+			}
+			else
+			{
+				_speed = targetSpeed;
+			}
+
+			// normalise input direction
+			Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+
+			// note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+			// if there is a move input rotate player when the player is moving
+			if (_input.move != Vector2.zero)
+			{
+				// move
+				inputDirection = transform.right * _input.move.x + transform.forward * _input.move.y;
+			}
+
+			// move the player
+			_controller.Move(inputDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+		}
+
+		private void JumpAndGravity()
+		{
+			if (Grounded)
+			{
+				// reset the fall timeout timer
+				_fallTimeoutDelta = FallTimeout;
+
+				// stop our velocity dropping infinitely when grounded
+				if (_verticalVelocity < 0.0f)
+				{
+					_verticalVelocity = -2f;
+				}
+
+				// Jump
+				if (_input.jump && _jumpTimeoutDelta <= 0.0f)
+				{
+					// the square root of H * -2 * G = how much velocity needed to reach desired height
+					_verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+				}
+
+				// jump timeout
+				if (_jumpTimeoutDelta >= 0.0f)
+				{
+					_jumpTimeoutDelta -= Time.deltaTime;
+				}
+			}
+			else
+			{
+				// reset the jump timeout timer
+				_jumpTimeoutDelta = JumpTimeout;
+
+				// fall timeout
+				if (_fallTimeoutDelta >= 0.0f)
+				{
+					_fallTimeoutDelta -= Time.deltaTime;
+				}
+
+				// if we are not grounded, do not jump
+				_input.jump = false;
+			}
+
+			// apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
+			if (_verticalVelocity < _terminalVelocity)
+			{
+				_verticalVelocity += Gravity * Time.deltaTime;
+			}
+		}
+
+		private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+		{
+			if (lfAngle < -360f) lfAngle += 360f;
+			if (lfAngle > 360f) lfAngle -= 360f;
+			return Mathf.Clamp(lfAngle, lfMin, lfMax);
+		}
+
+		private void OnDrawGizmosSelected()
+		{
+			Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
+			Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
+
+			if (Grounded) Gizmos.color = transparentGreen;
+			else Gizmos.color = transparentRed;
+
+			// when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
+			Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z), GroundedRadius);
+		}
+	}
+}
---
+++ EngineForks/WorkaroundStarterAssetsDeletedInputStarterAssetsInputs.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+namespace PublishersFork
+{
+	/// <summary>
+	/// NOTE: This REQUIRES you to also use the class <see cref="WorkaroundStarterAssetsDeletedInputFirstPersonController"/>
+	/// since Unity's design splits the implementation into two classes.
+	///
+	/// NOTE: If you want to support both old and new input then it is easy to take the modifications in this
+	/// pair of classes and add them to Unity's official classes - use "diff" to see exactly which lines of code
+	/// needed modification. Unity staff could have (should have!) done this themselves but chose not to.
+	/// </summary>
+
+	public class WorkaroundStarterAssetsDeletedInputStarterAssetsInputs : MonoBehaviour
+	{
+		[Header("Character Input Values")]
+		public Vector2 move;
+		public Vector2 look;
+		public bool jump;
+		public bool sprint;
+
+		[Header("Movement Settings")]
+		public bool analogMovement;
+
+		[Header("Mouse Cursor Settings")]
+		public bool cursorLocked = true;
+		public bool cursorInputForLook = true;
+
+		public void SampleInput()
+		{
+			move = Vector2.zero;
+			if( Input.GetKey( KeyCode.LeftArrow  ) || Input.GetKey( KeyCode.A ) )
+				move.x += -1f;
+			if( Input.GetKey( KeyCode.RightArrow ) || Input.GetKey( KeyCode.D ))
+				move.x += 1f;
+			
+			if( Input.GetKey( KeyCode.UpArrow ) || Input.GetKey( KeyCode.W ) )
+				move.y += 1f;
+			if( Input.GetKey( KeyCode.DownArrow ) || Input.GetKey( KeyCode.S ))
+				move.y += -1f;
+			
+			look.x = Input.GetAxis( "Mouse X" );
+			look.y = Input.GetAxis( "Mouse Y" );
+
+			sprint = Input.GetKey( KeyCode.LeftShift );
+			jump = Input.GetKeyDown( KeyCode.Space );
+		}
+
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+		public void OnMove(InputValue value)
+		{
+			MoveInput(value.Get<Vector2>());
+		}
+
+		public void OnLook(InputValue value)
+		{
+			if(cursorInputForLook)
+			{
+				LookInput(value.Get<Vector2>());
+			}
+		}
+
+		public void OnJump(InputValue value)
+		{
+			JumpInput(value.isPressed);
+		}
+
+		public void OnSprint(InputValue value)
+		{
+			SprintInput(value.isPressed);
+		}
+#endif
+
+
+		public void MoveInput(Vector2 newMoveDirection)
+		{
+			move = newMoveDirection;
+		} 
+
+		public void LookInput(Vector2 newLookDirection)
+		{
+			look = newLookDirection;
+		}
+
+		public void JumpInput(bool newJumpState)
+		{
+			jump = newJumpState;
+		}
+
+		public void SprintInput(bool newSprintState)
+		{
+			sprint = newSprintState;
+		}
+		
+		private void OnApplicationFocus(bool hasFocus)
+		{
+			SetCursorState(cursorLocked);
+		}
+
+		private void SetCursorState(bool newState)
+		{
+			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+		}
+	}
+}
