79fa823c8cd4e52094c235e6f8562f482d9add2c
adamgit <adam.m.s.martin+github@gmail.com>
2023-02-26 23:01:13 +0000

    Updated to work even from inside a DLL (it no longer uses #if detection, but instead parses the global UnityVersion number)

--- EditorForks/WorkaroundUnityUIToolkitBrokenObjectSelector.cs
+++ EditorForks/WorkaroundUnityUIToolkitBrokenObjectSelector.cs
@@ -8,17 +8,6 @@ using Object = UnityEngine.Object;
 
 namespace UnityEditorForks
 {
-	/// <summary>
-	/// The UIToolkit team broke the core API "ShowObjectPicker", implemented their own version, and then made it 'internal'
-	/// to prevent anyone else from  using the code. So we have to do this painful workaround to access the API call they're
-	/// supposed to make public but 'forgot' to.
-	///
-	/// Usage:
-	/// e.g. to pick a Material:
-	///    ((Material)null).ShowObjectPicker<Material>( o => { Debug.Log( "selector closed"+o.name ); }, o => { Debug.Log( "selector updated"+o.name ); } );
-	///
-	/// (or you can directly access the static method using the full class name here) 
-	/// </summary>
 	public static class WorkaroundUnityUIToolkitBrokenObjectSelector
 	{
 		public enum ObjectPickerSources
@@ -35,55 +24,62 @@ namespace UnityEditorForks
 
 		public static void ShowObjectPicker<T>( Action<T> OnSelectorClosed, Action<T> OnSelectionChanged, T initialValueOrNull = null, ObjectPickerSources sources = ObjectPickerSources.ASSETS ) where T : UnityEngine.Object
 		{
-			var hiddenType = typeof(Editor).Assembly.GetType( "UnityEditor.ObjectSelector" );
+			var hiddenType = typeof(UnityEditor.Editor).Assembly.GetType( "UnityEditor.ObjectSelector" );
 			var ps = hiddenType.GetProperties( BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Public | BindingFlags.GetProperty );
 			//Debug.Log( "all props on "+(typeof(ObjectSelector))+": "+string.Join( ",\n", ps.Select( info => info.ToString() ) ) );
 			PropertyInfo piGet = hiddenType.GetProperty( "get", BindingFlags.Public | BindingFlags.Static );
 			var os = piGet.GetValue( null );
 
 			MethodInfo miShow = null;
-			//if( miShow == null )
-#if UNITY_2020_1_OR_NEWER
-		/*** Type 2: Unity ? - 2020 */
-		miShow = hiddenType.GetMethod( "Show", BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[]
-		{
-			typeof(T),
-			typeof(	System.Type),
-			typeof(UnityEngine.Object),
-			typeof(bool),
-			typeof(List<int>),
-			typeof(Action<UnityEngine.Object>),
-			typeof(Action<UnityEngine.Object>)
-		}, new ParameterModifier[0] );
-
-#else
-			/*** Type 1: Unity 2019 - ? */
-			miShow = hiddenType.GetMethod( "Show", BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[]
-			{
-				typeof(T),
-				typeof(System.Type),
-				typeof(SerializedProperty),
-				typeof(bool),
-				typeof(List<int>),
-				typeof(Action<UnityEngine.Object>),
-				typeof(Action<UnityEngine.Object>)
-			}, new ParameterModifier[0] );
-#endif
+			
+			var unityVersion = UnityEngine.Application.unityVersion.Split( '.' );
 
 			/**
-			 * Something went wrong ... did Unity staff change the API again? (already done it once...)
+			 * If definitely Unity-2019 or earlier, use the old version; for all others use the new version (so that this
+			 * is forwards-compatible with when Unity Marketing inevitably breaks all Unity version comparisons AGAIN while
+			 * the Unity Engineering team AGAIN fails to provide a working 'UnityVersion' API (come on, guys! It's not hard!
+			 * and only YOU can maintain it correctly, since you're the ones putting incomparable values into it!)
+			 */
+			if( int.TryParse( unityVersion[0], out int unityMajorVersion ) && unityMajorVersion < 2020 )
+			{
+				/** Type 1: Unity - up to 2019 */
+				miShow = hiddenType.GetMethod( "Show", BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[]
+				{
+					typeof(T),
+					typeof(System.Type),
+					typeof(SerializedProperty),
+					typeof(bool),
+					typeof(List<int>),
+					typeof(Action<UnityEngine.Object>),
+					typeof(Action<UnityEngine.Object>)
+				}, new ParameterModifier[0] );
+			}
+			else
+			{
+				/*** Type 2: Unity - 2020 onwards */
+				miShow = hiddenType.GetMethod( "Show", BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[]
+				{
+					typeof(T),
+					typeof(System.Type),
+					typeof(UnityEngine.Object),
+					typeof(bool),
+					typeof(List<int>),
+					typeof(Action<UnityEngine.Object>),
+					typeof(Action<UnityEngine.Object>)
+				}, new ParameterModifier[0] );
+			}
+
+			/**
+			 * Something went wrong ... did Unity staff add a third class? Argh!
 			 */
 			if( miShow == null )
 			{
 				string methodName = "Show";
 				Debug.LogError( "Found \"" + methodName + "\"methods: \n" + string.Join( ",\n", hiddenType.GetMethods( BindingFlags.NonPublic | BindingFlags.Instance )
-					                .Where( info => info.Name == methodName ).Select( info => "  " + info.Name + " {" + string.Join( ",\n      ", info.GetParameters().Select( parameterInfo => parameterInfo.Name + ":" + parameterInfo.ParameterType ) ) + "\n}\n" ) ) );
+					.Where( info => info.Name == methodName ).Select( info => "  " + info.Name + " {" + string.Join( ",\n      ", info.GetParameters().Select( parameterInfo => parameterInfo.Name + ":" + parameterInfo.ParameterType ) ) + "\n}\n" ) ) );
 
 /*			Debug.LogError("Found methods: "+string.Join(",\n", hiddenType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance).Select(info => info.Name+"{"+
-			                                                                                                                                    string.Join(", ",info.GetParameters().Select(
-				                                                                                                                                    parameterInfo => parameterInfo.Name+":"+parameterInfo.ParameterType))+"}")));
-				                                                                                                                                    
-				                                                                                                                                    */
+			                                                                                                                                   */
 			}
 
 			Action<UnityEngine.Object> onSelectorClosed;
